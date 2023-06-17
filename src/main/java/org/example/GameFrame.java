4b98cdb9aafeeb9813e8e4c1b54e01165a37c96e
Danylo <nikolenfk@gmail.com>
2023-06-17 19:49:36 +0200

    Change

---
+++ src/main/java/org/example/GameFrame.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.JFrame;
+import java.io.Serial;
+
+public class GameFrame extends JFrame {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    protected GameFrame() {
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
